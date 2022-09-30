6d29b4a6ef7d0426fc94217f152eb3cea7bc807e
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-09-30 01:19:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+module.exports = app;
